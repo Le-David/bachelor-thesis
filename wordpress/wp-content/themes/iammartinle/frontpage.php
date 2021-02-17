f7a6ae186b0f809fd91dc3b653e6add4d8f9840a
Quang Dat Le <lequangdatcz@gmail.com>
2021-02-18 00:08:44 +0100

    frontpage: set up a template

---
+++ wordpress/wp-content/themes/iammartinle/frontpage.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: Frontpage
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+$context = Timber::context();
+
+$timber_post     = new Timber\Post();
+$context['post'] = $timber_post;
+Timber::render( array( 'page-' . $timber_post->post_name . '.twig', 'frontpage.twig' ), $context );
