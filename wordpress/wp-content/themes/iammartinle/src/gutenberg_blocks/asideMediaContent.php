56acf6a8ccecb81c8bcddc158a3300e9b58cd95b
Quang Dat Le <lequangdatcz@gmail.com>
2021-02-25 00:29:35 +0100

    register gutenberg block ("asideMediaContent") to admin

--- wordpress/wp-content/themes/iammartinle/src/gutenberg_blocks/asideMediaContent.php
+++ wordpress/wp-content/themes/iammartinle/src/gutenberg_blocks/asideMediaContent.php
@@ -4,7 +4,19 @@
  * Custom gutenberg "aisdeMediaContent" block
  */
 
-function aside_media_content(){
+function aside_media_content() {
+
+    wp_register_script(
+        'aside-media-content', // $handle - (string)(Required) Name of the script.
+        get_template_directory_uri() . '/static/scripts/gutenberg_blocks/asideMediaContent.js', // $src - (string|bool) (Required) Full URL of the script, or path of the script relative to the WordPress root directory.
+        array( 'wp-i18n', 'wp-block', 'wp-block-editor' ) // $deps - (string[]) (Optional) An array of registered script handles this script depends on.
+    );
+
+    register_block_type(
+    'gutenberg-block/aside-media-content', // namespace/block-name - (Required structure block name) the namespace and name/slug of the block
+    array(
+        'editor_script' => 'aside-media-content', // assign the name of the registered script to "editor_script" to handle the block
+    ) );
 }
 
-add_action( 'after_setup_theme', 'aside_media_content' );
+add_action( 'init', 'aside_media_content' ); // call function "aside_media_content" when templates initialize
---
+++ wordpress/wp-content/themes/iammartinle/static/scripts/gutenberg_blocks/asideMediaContent.js
@@ -0,0 +1,15 @@
+const { __ } = wp.i18n
+const { registerBlockType } = wp.block
+const { useBlockProps } = wp.block-editor
+
+registerBlockType('gutenberg-block/aside-media-content', {
+    apiVersion: 2, // enabling blocks to render their own block wrapper element
+    title: 'Aside media content',
+    icon: 'align-left', // icon used in admin section
+    category: 'layout', // under which category will be find
+    keywords: [ __( 'media' ), __( 'image' ), __( 'photo' ), __( 'content' ), __( 'text' ) ], // another aliases when need to find certain block
+
+    edit() {}, // edit function which handles editation part (the part in wordpress - for example add/change text, picture, etc.)
+
+    save() {}, // save function which handles saved part (the part which is rendered on frontend - website)
+})
