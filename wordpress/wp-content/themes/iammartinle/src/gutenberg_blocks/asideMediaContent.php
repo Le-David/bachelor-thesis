0e0e470a344803cca4ae6a47dcf01489143ba1fd
Quang Dat Le <lequangdatcz@gmail.com>
2021-02-27 17:02:09 +0100

    path and file adjustments (necessary for building/watching scripts via npm)

--- wordpress/wp-content/themes/iammartinle/src/gutenberg_blocks/asideMediaContent.php
+++ wordpress/wp-content/themes/iammartinle/src/gutenberg_blocks/asideMediaContent.php
@@ -8,15 +8,16 @@ function aside_media_content() {
 
     wp_register_script(
         'aside-media-content', // $handle - (string)(Required) Name of the script.
-        get_template_directory_uri() . '/static/scripts/gutenberg_blocks/asideMediaContent.js', // $src - (string|bool) (Required) Full URL of the script, or path of the script relative to the WordPress root directory.
+        get_template_directory_uri() . '/build/scripts/index.js', // $src - (string|bool) (Required) Full URL of the script, or path of the script relative to the WordPress root directory.
         array( 'wp-i18n', 'wp-blocks' ) // $deps - (string[]) (Optional) An array of registered script handles this script depends on.
     );
 
     register_block_type(
-    'gutenberg-block/aside-media-content', // namespace/block-name - (Required structure block name) the namespace and name/slug of the block
-    array(
-        'editor_script' => 'aside-media-content', // assign the name of the registered script to "editor_script" to handle the block
-    ) );
+        'gutenberg-block/aside-media-content', // namespace/block-name - (Required structure block name) the namespace and name/slug of the block
+        array(
+            'editor_script' => 'aside-media-content', // assign the name of the registered script to "editor_script" to handle the block
+        )
+    );
 }
 
 add_action( 'init', 'aside_media_content' ); // call function "aside_media_content" when templates initialize
--- wordpress/wp-content/themes/iammartinle/static/scripts/gutenberg_blocks/index.js
+++ wordpress/wp-content/themes/iammartinle/static/scripts/gutenberg_blocks/index.js
@@ -8,7 +8,11 @@ registerBlockType('gutenberg-block/aside-media-content', {
     category: 'layout', // under which category will be find
     keywords: [ __( 'media' ), __( 'image' ), __( 'photo' ), __( 'content' ), __( 'text' ) ], // another aliases when need to find certain block
 
-    edit() {}, // edit function which handles editation part (the part in wordpress - for example add/change text, picture, etc.)
+    edit() { // edit function which handles editation part (the part in wordpress - for example add/change text, picture, etc.)
+        return <div>Hello world! (edit)</div>
+    },
 
-    save() {}, // save function which handles saved part (the part which is rendered on frontend - website)
+    save() { // save function which handles saved part (the part which is rendered on frontend - website)
+        return <div>Hello world! (save)</div>
+    },
 })
