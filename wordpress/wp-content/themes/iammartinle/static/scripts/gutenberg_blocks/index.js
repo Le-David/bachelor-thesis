3b589ddaccd8c914cc53674a32633c9bb82e7f7b
Quang Dat Le <lequangdatcz@gmail.com>
2021-02-27 18:15:37 +0100

    use default class for custom block

--- wordpress/wp-content/themes/iammartinle/static/scripts/gutenberg_blocks/index.js
+++ wordpress/wp-content/themes/iammartinle/static/scripts/gutenberg_blocks/index.js
@@ -10,11 +10,15 @@ registerBlockType('gutenberg-block/aside-media-content', {
     category: 'layout', // under which category will be find
     keywords: [ __( 'media' ), __( 'image' ), __( 'photo' ), __( 'content' ), __( 'text' ) ], // another aliases when need to find certain block
 
-    edit() { // edit function which handles editation part (the part in wordpress - for example add/change text, picture, etc.)
-        return <div>Hello world! (edit)</div>
+    edit: (props) => { // edit function which handles editation part (the part in wordpress - for example add/change text, picture, etc.)
+        const blockProps = useBlockProps();
+
+        return <p { ...blockProps }>Hello world! (edit)</p>
     },
 
-    save() { // save function which handles saved part (the part which is rendered on frontend - website)
-        return <div>Hello world! (save)</div>
+    save: (props) => { // save function which handles saved part (the part which is rendered on frontend - website)
+        const blockProps = useBlockProps.save();
+
+        return <p { ...blockProps }>Hello world! (save)</p>
     },
 })
