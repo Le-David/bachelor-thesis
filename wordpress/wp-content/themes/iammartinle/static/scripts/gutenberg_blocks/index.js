aed5535ecb2014a0a8095c6d00f176ad13b19ae0
Quang Dat Le <lequangdatcz@gmail.com>
2021-03-01 00:30:13 +0100

    add "content" side to "asideMediaContent"

--- wordpress/wp-content/themes/iammartinle/static/scripts/gutenberg_blocks/index.js
+++ wordpress/wp-content/themes/iammartinle/static/scripts/gutenberg_blocks/index.js
@@ -1,6 +1,6 @@
 const { __ } = wp.i18n
 const { registerBlockType } = wp.blocks
-const { useBlockProps } = wp.blockEditor
+const { useBlockProps, RichText } = wp.blockEditor
 
 registerBlockType('gutenberg-block/aside-media-content', {
     apiVersion: 2,
@@ -10,15 +10,69 @@ registerBlockType('gutenberg-block/aside-media-content', {
     category: 'layout', // under which category will be find
     keywords: [ __( 'media' ), __( 'image' ), __( 'photo' ), __( 'content' ), __( 'text' ) ], // another aliases when need to find certain block
 
+    attributes: {
+        title: {
+            type: 'string',
+            source: 'html',
+            selector: 'h2'
+        },
+        content: {
+            type: 'string',
+            source: 'html',
+            selector: 'p'
+        }
+    },
+
     edit: (props) => { // edit function which handles editation part (the part in wordpress - for example add/change text, picture, etc.)
+
+        const {
+            attributes: {
+                title, 
+                content,
+            },
+            setAttributes,
+        } = props
+
         const blockProps = useBlockProps();
 
-        return <p { ...blockProps }>Hello world! (edit)</p>
+        return ([
+            <div { ...blockProps}>
+                <RichText
+                    tagName="h2"
+                    value={ title }
+                    placeholder="Add title..."
+                    onChange={ (title) => setAttributes({ title }) }
+                />
+                <RichText
+                    tagName="p"
+                    value={ content }
+                    placeholder="Add content..."
+                    onChange={ (content) => setAttributes({ content }) }
+                    multiline={ true }
+                />
+            </div>
+        ])
     },
 
     save: (props) => { // save function which handles saved part (the part which is rendered on frontend - website)
+
+        const {
+            attributes: {
+                title, 
+                content
+            },
+        } = props
+
         const blockProps = useBlockProps.save();
 
-        return <p { ...blockProps }>Hello world! (save)</p>
+        return (
+            <div { ...blockProps}>
+                <h2>{ title }</h2>
+                <RichText.Content
+                    tagName="div"
+                    value={ content }
+                />
+            </div>
+        )
     },
 })
