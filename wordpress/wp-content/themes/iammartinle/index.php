3a1cd07f1b73e35d519233f61a82f01dd0cc3a3a
Quang Dat Le <lequangdatcz@gmail.com>
2021-03-13 13:46:03 +0100

    set variables to default "posts page"

--- wordpress/wp-content/themes/iammartinle/index.php
+++ wordpress/wp-content/themes/iammartinle/index.php
@@ -13,10 +13,32 @@
  * @since   Timber 0.1
  */
 
-$context          = Timber::context();
-$context['posts'] = new Timber\PostQuery();
-$context['foo']   = 'bar';
-$templates        = array( 'index.twig' );
+$context = Timber::context();
+$excludedCategory = Timber::get_terms([
+    'taxonomy' => 'category',
+    'slug' => 'uncategorized',
+    ]);
+$totalPages = get_option('posts_per_page');
+
+$timber_post     = new Timber\Post();
+$context['post'] = $timber_post;
+$context['categories'] = Timber::get_terms([
+    'post_type' =>'post',
+    'post_status' =>'publish',
+    'taxonomy' => 'category',
+    'orderby' => 'menu_order',
+    'order' => 'ASC',
+    'posts_per_page' => $totalPages,
+    'exclude' => $excludedCategory[0]->id,
+]);
+$context['related_posts'] = Timber::get_posts([
+    'post_type' =>'post',
+    'post_status' =>'publish',
+    'order' => 'DESC',
+    'orderby' => 'date',
+    'posts_per_page' => $totalPages,
+]);
+$templates        = array( 'template-articles.twig' ,'index.twig' );
 if ( is_home() ) {
 	array_unshift( $templates, 'front-page.twig', 'home.twig' );
 }
